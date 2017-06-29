bcddc776c15348abd4e050fd1803882b194604b1
orrlevinger <orrl@gefen.online>
2017-06-29 11:32:31 +0300

    mongo node first commit

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,25 @@
+const MongoClient = require('mongodb').MongoClient;
+
+let mongo;
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if (err) {
+        return console.log('failed to connect to mongodb');
+    }
+    console.log('connected to mongodb');
+    mongo = db;
+    db.collection('Todos').insertOne({
+            text: 'something to do',
+            done: false
+        },
+        (err, res) => {
+            if (err) {
+                console.log('unable to insert record', err);
+            } else {
+                console.log('insert record successfully');
+            }
+        });
+
+    db.close();
+});
+
