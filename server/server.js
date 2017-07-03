1051eeb4c3bd59e47cd91569b0c449de1229e74d
orrlevinger <orrl@gefen.online>
2017-07-03 16:26:31 +0300

    added mongo uri

--- server/server.js
+++ server/server.js
@@ -1,3 +1,12 @@
+const env = process.env.NODE_ENV || 'development';
+
+if(env === 'test'){
+    process.env.MONGODB_URI='mongodb://localhost:27017/TodoAppTest';
+}else if(env === 'development'){
+    process.env.MONGODB_URI='mongodb://localhost:27017/TodoApp';
+}
+
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
